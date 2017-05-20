297b24149354b4d11370e3dd3d1beb807df1a581
Shamila <shamila.m09@gmail.com>
2017-05-20 16:45:13 -0700

    configuring routes for heroku

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var app = express();
 var bodyParser	= require('body-parser');
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
+// app.use(express.static(__dirname + '/client'));
 
+
+// for heroku 1
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
+});
 
 // for heroku
 app.listen(process.env.PORT || 8000);
