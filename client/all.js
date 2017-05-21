d74a5ace6295788dda891f4fe6676856fb79ccca
Shamila <shamila.m09@gmail.com>
2017-05-20 21:17:34 -0700

    in progress

--- client/all.js
+++ client/all.js
@@ -1,6 +1,6 @@
 ( function (angular) {
 	angular
-		.module('portfolioApp', ['ui.router','ui.bootstrap','ngAnimate', 'ngMaterial']);
+		.module('portfolioApp', ['ui.router','ui.bootstrap']);
 } )(angular);
 		
 		
